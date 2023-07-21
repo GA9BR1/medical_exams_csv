8be9209192293f20e669e33b618dae5600b4c214
ga9br1 <gustavoalberttodev@gmail.com>
2023-07-20 22:39:17 -0300

    Organização do código

--- insertions.rb
+++ insertions.rb
@@ -2,61 +2,80 @@ require 'pg'
 require 'json'
 require 'debug'
 class Insertions
-  def self.insert
+  def self.insert_csv_data
     db = PG.connect(host: 'postgres-server', user: 'postgres')
-    rows = CSV.read("./data.csv", col_sep: ';')
-    columns = rows.shift
-    json = rows.map do |row|
-              row.each_with_object({}).with_index do |(cell, acc), idx|
-                column = columns[idx]
-                acc[column] = cell
-              end
-            end.to_json
-    data = JSON.parse(json)
+    data = self.read_and_parse_csv_to_json
 
     data.each do |object|
-      patient_not_exists = db.exec('SELECT * FROM patients WHERE cpf = $1', [object['cpf']]).num_tuples.zero?
-      if patient_not_exists
-        patient_id = db.exec('INSERT INTO patients (name, cpf, email, birthday, address, city, state) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-                     [object['nome paciente'], object['cpf'], object['email paciente'], 
-                     object['data nascimento paciente'], object['endereço/rua paciente'],
-                     object['cidade paciente'], object['estado patiente']])
-      end
+      self.insert_patient_if_not_exists(object, db)
+      self.insert_doctor_if_not_exists(object, db)
+      self.insert_test_type_if_not_exists(object, db)
+      self.insert_test_and_test_items_if_not_exist(object, db)
+    end
+  end
 
-      doctor_not_exists = db.exec('SELECT * FROM doctors WHERE crm = $1', [object['crm médico']]).num_tuples.zero?
+  private
+  
+    def self.read_and_parse_csv_to_json
+      rows = CSV.read("./data.csv", col_sep: ';')
+      columns = rows.shift
+      json = rows.map do |row|
+                row.each_with_object({}).with_index do |(cell, acc), idx|
+                  column = columns[idx]
+                  acc[column] = cell
+                end
+              end.to_json
+      data = JSON.parse(json)
+    end
+
+    def self.insert_patient_if_not_exists(object, db)
+      patient_not_exists = db.exec_params('SELECT * FROM patients WHERE cpf = $1', [object['cpf']]).num_tuples.zero?
+      if patient_not_exists
+        patient_id = db.exec_params('INSERT INTO patients (name, cpf, email, birthday, address, city, state) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+                    [object['nome paciente'], object['cpf'], object['email paciente'], 
+                    object['data nascimento paciente'], object['endereço/rua paciente'],
+                    object['cidade paciente'], object['estado patiente']])
+      end
+    end
+
+    def self.insert_doctor_if_not_exists(object, db)
+      doctor_not_exists = db.exec_params('SELECT * FROM doctors WHERE crm = $1', [object['crm médico']]).num_tuples.zero?
       if doctor_not_exists
-        db.exec('INSERT INTO doctors (name, crm, crm_state, email) VALUES ($1, $2, $3, $4)',
+        db.exec_params('INSERT INTO doctors (name, crm, crm_state, email) VALUES ($1, $2, $3, $4)',
                 [object['nome médico'], object['crm médico'], object['crm médico estado'], 
                 object['email médico']])
       end
+    end
 
-      test_type_not_exists = db.exec('SELECT * FROM test_types WHERE name = $1', [object['tipo exame']]).num_tuples.zero?
+    def self.insert_test_type_if_not_exists(object, db)
+      test_type_not_exists = db.exec_params('SELECT * FROM test_types WHERE name = $1', [object['tipo exame']]).num_tuples.zero?
       if test_type_not_exists
-        db.exec('INSERT INTO test_types (name, limits) VALUES ($1, $2) RETURNING id',
+        db.exec_params('INSERT INTO test_types (name, limits) VALUES ($1, $2) RETURNING id',
         [object['tipo exame'], object['limites tipo exame']])
       end
-
-      test_not_exists = db.exec('SELECT * FROM tests WHERE token = $1', [object['token resultado exame']]).num_tuples.zero?
-      test_type_id = db.exec('SELECT id FROM test_types WHERE name = $1', [object['tipo exame']])[0]['id'].to_i
-      if test_not_exists
-        patient_id = db.exec('SELECT id FROM patients WHERE cpf = $1', [object['cpf']])[0]['id'].to_i
-        doctor_id = db.exec('SELECT id FROM doctors WHERE crm = $1', [object['crm médico']])[0]['id'].to_i
-        db.exec('INSERT INTO tests (token, patient_id, doctor_id, date) VALUES ($1, $2, $3, $4)',
-                [object['token resultado exame'], patient_id, doctor_id, object['data exame']])
-        test_id = db.exec('SELECT * FROM tests WHERE token = $1', [object['token resultado exame']])[0]['id'].to_i
-
-        db.exec('INSERT INTO test_items (result, test_type_id, test_id) VALUES ($1, $2, $3)',
-                [object['resultado tipo exame'], test_type_id, test_id])
-      else
-        test_id = db.exec('SELECT * FROM tests WHERE token = $1', [object['token resultado exame']])[0]['id'].to_i
-        test_item_not_exists = db.exec('SELECT * FROM test_items ti
-                               JOIN test_types tt ON ti.test_type_id = tt.id
-                               WHERE ti.test_id = $1 AND tt.name = $2', [test_id, object['tipo exame']]).num_tuples.zero?
-        if test_item_not_exists
-          db.exec('INSERT INTO test_items (result, test_type_id, test_id) VALUES ($1, $2, $3)',
-                [object['resultado tipo exame'], test_type_id, test_id])
-        end
-      end
     end
-  end
+
+    def self.insert_test_and_test_items_if_not_exist(object, db)
+      test_not_exists = db.exec_params('SELECT * FROM tests WHERE token = $1', [object['token resultado exame']]).num_tuples.zero?
+      test_type_id = db.exec_params('SELECT id FROM test_types WHERE name = $1', [object['tipo exame']])[0]['id'].to_i
+        if test_not_exists
+          patient_id = db.exec_params('SELECT id FROM patients WHERE cpf = $1', [object['cpf']])[0]['id'].to_i
+          doctor_id = db.exec_params('SELECT id FROM doctors WHERE crm = $1', [object['crm médico']])[0]['id'].to_i
+          db.exec_params('INSERT INTO tests (token, patient_id, doctor_id, date) VALUES ($1, $2, $3, $4)',
+                  [object['token resultado exame'], patient_id, doctor_id, object['data exame']])
+          test_id = db.exec_params('SELECT * FROM tests WHERE token = $1', [object['token resultado exame']])[0]['id'].to_i
+
+          db.exec_params('INSERT INTO test_items (result, test_type_id, test_id) VALUES ($1, $2, $3)',
+                  [object['resultado tipo exame'], test_type_id, test_id])
+        else
+          test_id = db.exec_params('SELECT * FROM tests WHERE token = $1', [object['token resultado exame']])[0]['id'].to_i
+          test_item_not_exists = db.exec_params('SELECT * FROM test_items ti
+                                                 JOIN test_types tt ON ti.test_type_id = tt.id
+                                                 WHERE ti.test_id = $1 AND tt.name = $2', [test_id, object['tipo exame']]).num_tuples.zero?
+          if test_item_not_exists
+            db.exec_params('INSERT INTO test_items (result, test_type_id, test_id) VALUES ($1, $2, $3)',
+                  [object['resultado tipo exame'], test_type_id, test_id])
+          end
+        end
+    end
 end
---
+++ query_and_format.rb
@@ -0,0 +1,90 @@
+class QueryAndFormat
+  def self.get_all_tests
+    query = self.query_all_tests
+    json = self.format_to_json(query)
+    final = self.group_tests(json)
+    JSON.pretty_generate(final)
+  end
+
+  private 
+
+  def self.query_all_tests
+    db = PG.connect(host: 'postgres-server', user: 'postgres')
+    db.exec("SELECT
+      t.token AS result_token,
+      t.date AS result_date, 
+      p.cpf, 
+      p.name, 
+      p.email, 
+      p.birthday,
+      json_build_object(
+        'crm', d.crm,
+        'crm_state', d.crm_state,
+        'name', d.name
+                      ) AS doctor,
+        json_build_object(
+          'type', tt.name,
+          'limits', tt.limits,
+          'result', ti.result
+        ) AS tests
+      FROM tests AS t
+      JOIN patients p ON t.patient_id = p.id
+      JOIN doctors d ON t.doctor_id = d.id
+      JOIN test_items ti ON ti.test_id = t.id
+      JOIN test_types tt ON ti.test_type_id = tt.id
+      GROUP BY t.id, t.date, p.cpf, p.name, p.email, p.birthday, d.crm, d.crm_state, d.name, tt.name, tt.limits, ti.id
+    ").to_a
+  end 
+
+  def self.format_to_json(data)
+    data.map do |row|
+      {
+        'result_token' => row['result_token'],
+        'result_date' => row['result_date'],
+        'cpf' => row['cpf'],
+        'name' => row['name'],
+        'email' => row['email'],
+        'birthday' => row['birthday'],
+        'doctor' => JSON.parse(row['doctor']),
+        'tests' => JSON.parse(row['tests'])
+      }
+    end
+  end
+
+  def self.group_tests(data)
+    grouped_data = []
+    data.each do |record|
+      token = record['result_token']
+      found_index = nil
+      grouped_data.each_with_index do |item, index|
+        if item['result_token'] == token
+          found_index = index
+          break
+        end
+      end
+      if found_index
+        grouped_data[found_index]['tests'] << {
+          'type' => record['tests']['type'],
+          'limits' => record['tests']['limits'],
+          'result' => record['tests']['result']
+        }
+      else 
+        grouped_data << {
+        'result_token' => token,
+        'result_date' => record['result_date'],
+        'cpf' => record['cpf'],
+        'name' => record['name'],
+        'email' => record['email'],
+        'birthday' => record['birthday'],
+        'doctor' => record['doctor'],
+        'tests' => [{
+          'type' => record['tests']['type'],
+          'limits' => record['tests']['limits'],
+          'result' => record['tests']['result']
+        }]
+      }
+      end
+    end
+    grouped_data
+  end
+end
